9bb27f7000542fd8bb5964ef47a48994b9fa6b22
Song <songlip.lim@redalphacyber.com>
2025-08-08 15:20:36 +0800

    Removed a line from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ VENV_DIR="$APP_DIR/venv"
 mkdir -p "$APP_DIR"
 chown -R administrator:administrator "$APP_DIR"
 
-cp -r ./* "$APP_DIR/"
+#cp -r ./* "$APP_DIR/"
 
 if [ ! -d "$VENV_DIR" ]; then
   python3 -m venv "$VENV_DIR"
